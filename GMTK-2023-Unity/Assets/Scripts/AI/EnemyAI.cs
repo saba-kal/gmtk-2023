f9e62e90414cadb32bd927cfe303cccf4e5728c7
saba-kal <38592380+saba-kal@users.noreply.github.com>
2023-07-07 17:31:14 -0400

    Implemented basic AI.

---
+++ GMTK-2023-Unity/Assets/Scripts/AI/EnemyAI.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEngine.AI;
+
+[RequireComponent(typeof(NavMeshAgent))]
+public class EnemyAI : MonoBehaviour
+{
+    [SerializeField] private GameObject _target;
+
+    private NavMeshAgent _agent;
+
+    private void Start()
+    {
+        _agent = GetComponent<NavMeshAgent>();
+    }
+
+    private void Update()
+    {
+        _agent.SetDestination(_target.transform.position);
+    }
+}
